f3ace5b7f9701ae097c92ee3435aefc89fe9af67
XSR700 <89107047+XSR700@users.noreply.github.com>
2021-11-02 14:54:17 -0500

    Add files via upload

--- Schema.sql
+++ Schema.sql
@@ -1,6 +1,3 @@
-SQL
-
-
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -40,3 +37,21 @@ CREATE TABLE salaries (
 );
 
 SELECT * FROM departments;
+
+CREATE TABLE dept_emp (
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE Titles (
+  emp_no INT NOT NULL,
+  title VARCHAR(40) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
