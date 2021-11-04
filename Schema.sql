7f0e6337bdc7478c8ef38949d1f402df588383f7
XSR700 <89107047+XSR700@users.noreply.github.com>
2021-11-04 13:09:22 -0500

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,4 +1,5 @@
 -- Creating tables for PH-EmployeeDB
+DROP TABLE departments;
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
@@ -6,7 +7,10 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
+SELECT * FROM departments;
+
 --Employees Table Create
+DROP TABLE employees;
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
     birth_date DATE NOT NULL,
@@ -17,16 +21,22 @@ CREATE TABLE employees (
     PRIMARY KEY (emp_no)
 );
 
+SELECT * FROM employees;
+
+DROP TABLE dept_emp;
 CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+SELECT * FROM dept_emp;
+
+DROP TABLE salaries;
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
@@ -36,9 +46,10 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM dept_emp;
+SELECT * FROM salaries;
 
-CREATE TABLE Managers (
+DROP TABLE dept_managers;
+CREATE TABLE dept_managers (
 	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
@@ -48,11 +59,15 @@ CREATE TABLE Managers (
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE Titles (
+SELECT * FROM dept_managers;
+
+DROP TABLE titles;
+CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)		
 );
+
+SELECT * FROM Titles;
